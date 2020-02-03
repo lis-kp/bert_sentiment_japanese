59f3f9e0ee3d7c39ee6218519a7de997bd1d4f48
Lis Kanashiro <lisk@Liss-MacBook-Pro.local>
2020-02-03 17:24:54 +0900

    first commit

---
+++ sentiment_bert/bert_data_processor_ja.py
@@ -0,0 +1,63 @@
+from torch.utils.data import Dataset
+from transformers import BertTokenizer
+import torch
+from transformers.tokenization_bert_japanese import BertJapaneseTokenizer
+
+# customized class for preparing datasets
+class BERTInputConverter(Dataset):
+    def __init__(self, sentences=None, labels=None, maxlen=128):
+
+        self.sentences = sentences
+        self.labels = labels
+
+        # Initialize the BERT tokenizer
+        self.tokenizer = BertJapaneseTokenizer.from_pretrained("bert-base-japanese-whole-word-masking")
+
+        self.maxlen = maxlen
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, index):
+
+        # Selecting the sentence and label at the specified index
+        sentence = self.sentences[index]
+        label = self.labels[index]
+
+        #get bert features
+        input_ids_tensor, input_mask_tensor, label_tensor = self.get_bert_features(sentence, label)
+
+        return input_ids_tensor, input_mask_tensor, label_tensor
+
+    def get_bert_features(self, sentence, label=None):
+        # Tokenize the sentence
+        tokenized_sentence = self.tokenizer.tokenize(sentence)
+
+        # Account for one [SEP] & one [CLS] with "- 2"
+        if len(tokenized_sentence) > self.maxlen - 2:
+            tokenized_sentence = tokenized_sentence[:self.maxlen - 2]
+
+        tokens = ['[CLS]'] + tokenized_sentence + [
+            '[SEP]']  # Insering the CLS and SEP token in the beginning and end of the sentence
+
+        input_ids = self.tokenizer.convert_tokens_to_ids(
+            tokens)  # Obtaining the indices of the tokens in the BERT Vocabulary
+
+        # The mask has 1 for real tokens and 0 for padding tokens.
+        input_mask = [1] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        while len(input_ids) < self.maxlen:
+            input_ids.append(0)
+            input_mask.append(0)
+
+        # Converting to pytorch tensors
+        input_ids_tensor = torch.tensor(input_ids)
+
+        input_mask_tensor = torch.tensor(input_mask)
+
+        if label is not None:
+            label_tensor = torch.tensor(label)
+            return input_ids_tensor, input_mask_tensor, label_tensor
+
+        return input_ids_tensor, input_mask_tensor
---
+++ sentiment_bert/eval_utils.py
@@ -0,0 +1,99 @@
+import numpy as np
+import torch
+from bert_data_processor_ja import BERTInputConverter
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def evaluate(model, dataloader):
+    # check if gpu is available
+    if torch.cuda.is_available:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    # Put the model in evaluation mode
+    model.eval()
+
+    # Tracking variables
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+
+    # Evaluate data for one epoch
+    for batch in dataloader:
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+
+        # Telling the model not to compute or store gradients
+        with torch.no_grad():
+            # Forward pass, calculate logit predictions.
+            outputs = model(b_input_ids,
+                            token_type_ids=None,
+                            attention_mask=b_input_mask)
+
+        # Get the "logits" output by the model
+        logits = outputs[0]
+
+        # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+
+        # Calculate the accuracy
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+        # Accumulate the total accuracy
+        eval_accuracy += tmp_eval_accuracy
+
+        # Track the number of batches
+        nb_eval_steps += 1
+
+    acc = eval_accuracy / nb_eval_steps
+    return acc
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def evaluate_single_sentence(model, sentence):
+    # check if gpu is available
+    if torch.cuda.is_available:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    bert = BERTInputConverter()
+    input_ids_tensor, input_mask_tensor = bert.get_bert_features(sentence)
+
+    # Put the model in evaluation mode
+    model.eval()
+
+    input_ids_tensor = input_ids_tensor.to(device)
+    input_mask_tensor = input_mask_tensor.to(device)
+
+    # Telling the model not to compute or store gradients
+    with torch.no_grad():
+        output = model(input_ids_tensor.unsqueeze(0),
+                                token_type_ids=None,
+                                attention_mask=input_mask_tensor.unsqueeze(0))
+
+    logits = output[0].detach().cpu().numpy()
+
+    predicted_label = np.argmax(logits, axis=1).flatten()
+
+    if predicted_label[0] == 0:
+        predicted_label = "Negative"
+    else:
+        predicted_label = "Positive"
+
+
+    probability = np.max(softmax(logits)[0])
+
+    return predicted_label, probability
+
